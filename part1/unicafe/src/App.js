5bf19f7fa40a949deb1d1120267aeabf6a92f7f9
Daniel Phillips <dan.phillips100@gmail.com>
2022-03-16 17:04:59 +0000

    Draft 1.11 Completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,9 +1,69 @@
-import { useState } from 'react';
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
+
+const StatisticLine = ({text, value, icon, text1, value1}) => {
+  return (
+    <tbody>
+    <tr>
+      <td>{text}</td>
+      <td>{value}{icon} {text1} {value1}</td>
+    </tr>
+    </tbody>
+  )
+}
+
+const Statistics = ({test}) => {
+  if (test.total === 0 || isNaN(test.total)) {
+    return (
+      <></>
+    )
+  }
+  return (
+    <table>
+      <StatisticLine text = "Good" value = {test.good} />
+      <StatisticLine text = "Neutral" value = {test.neutral} />
+      <StatisticLine text = "Bad" value = {test.bad} />
+      <StatisticLine text = "All" value = {test.total} />
+      <StatisticLine text = "Average" value = {test.average} text1 = "out of" value1 = {test.total} />
+      <StatisticLine text = "Positive" value = {test.positive} icon = "%" />
+    </table>
+  )
+}
 
 const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodClick = () => setGood(good + 1)
+  const neutralClick = () => setNeutral(neutral + 1)
+  const badClick = () => setBad(bad + 1)
+
+  const total = good + neutral + bad;
+
+  const test = {
+    good: good,
+    neutral: neutral,
+    bad: bad,
+    total: good + neutral + bad,
+    average: ((good*1) + (bad*-1)/ total).toFixed(2),
+    positive: ((good/total)*100).toFixed(2)
+  }
+
   return (
-    <div>
-      hello world
+    <div id="wrapper">
+      <h1>Unicafe</h1>
+      <h2>Please provide us with your feedback</h2>
+
+      <div id="buttonContainer">
+        <Button handleClick={goodClick} text = "Good"/>
+        <Button handleClick={neutralClick} text = "Neutral"/>
+        <Button handleClick={badClick} text = "Bad"/>
+      </div>
+
+      <Statistics test = {test} />
+
     </div>
   );
 }
