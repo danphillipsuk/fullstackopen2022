a40d0bcedcdfe518cdffa6ae39c83fe1a1c9c52e
Daniel Phillips <dan.phillips100@gmail.com>
2022-03-30 11:30:38 +0100

    Exercise 2.1 Complete

---
+++ part2/course-information/src/components/Content.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Part from './Part';
+
+const Content = ( props ) => {
+  const { parts } = props;
+  return (
+    <div>
+      <ul>
+        {parts.map(part =>
+          <Part key={part.id} part={part} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default Content
